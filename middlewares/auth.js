75c7ebf5564344d25b0b135c3efb8a1fb411d597
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-05-19 10:59:53 +0200

    fix_ errorers de sintaxys

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export function authMiddleware(req, res, next){
     try {
         const authHeader = req.headers.authorization
-        if(!authHeader || !authHeader.strtsWith('Bearer ')){
+        if(!authHeader || !authHeader.startsWith('Bearer ')){
             return res.status(401).json({ error: 'No autorizado'})
         }
 
