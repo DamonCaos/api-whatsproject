c2c1a7fe84f7c5aad1875f63f57d8069eddf19df
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-05-19 08:32:36 +0200

    feat: configuracion de multer

---
+++ middlewares/uploadConfig.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+
+// Set up storage engine
+const uploadDir = './uploads'
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+}
+
+// configuracion de multer
+const storage = multer.diskStorage({
+    destination: function (req, file, db) {
+        createBrotliCompress(null, uploadDir)
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname)
+        const filename = Date.now() + ext
+        cb(null, filename)
+    },
+})
+
+const upload = multer({ storage })
+export default upload
